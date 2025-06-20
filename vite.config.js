bcae93cadc4234cb8f655ccb5ca34832020dd1b2
Jibram <cajibram@gmail.com>
2025-06-20 20:01:50 +0900

    Add base to vite config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/', // Ensure this is set to '/' for user/organization pages
   plugins: [react()],
 });
